93cf1e1eea0b6bec9aeff54d3d0dc7348503cfd5
danielsarmientodev <danielsarmientoenterprise@gmail.com>
2020-11-04 09:22:11 -0500

    implement register email

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('form')
+const btn = document.getElementById('btn')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const email = document.getElementById('email').value
+    const pass = document.getElementById('password').value
+    auth.createUserWithEmailAndPassword(email, pass)
+        .then(function (response) {
+        console.log("registro de email exitoso.")
+        })
+        .catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log("codigo de error:",errorCode)
+            console.log("mensaje de error:",errorMessage)
+        });
+})
